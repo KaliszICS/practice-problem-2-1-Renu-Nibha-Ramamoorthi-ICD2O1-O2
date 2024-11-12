496ca5b2d482d6d42a0ceb1db46fe01d62e73c2a
Renu-Nibha-Ramamoorthi-ICD2O1-O2 <renunibha.ramamoorthi@student.tdsb.on.ca>
2024-11-12 13:17:59 -0500

    Update PP2_1.py

--- PP2_1.py
+++ PP2_1.py
@@ -2,11 +2,19 @@
 
 def q1(): 
   #Write Assignment code here
-
+  num = int(input("Enter a number: "))
+  if num % 2 == 0: 
+    print(f"{num} is even") 
+  else:
+    print(f"{num} is odd") 
 
 def q2(): 
   #Write Assignment code here
-
+  name = input("Enter a name:")
+  if name == "Kalisz":
+    print("teacher") 
+  else: 
+    print("student") 
 
 
 #Do not alter the following code
